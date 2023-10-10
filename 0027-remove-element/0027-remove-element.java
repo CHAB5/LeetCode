d8f176f06f690889d65d9dcab464c6f788bac0f3
CHAB5 <100051530+CHAB5@users.noreply.github.com>
2023-10-10 16:50:51 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (8.85%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,25 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         
-        int k=0;
-        int[] newnums = new int[nums.length];
-        int j=0;
+        int index=0;
         
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == val){
-                k++;
-            }
-            else{
-                newnums[j]=nums[i];
-                j++;
+        for(int i=0;i<nums.length; i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
             }
         }
         
-        for(int i=0;i<newnums.length; i++){
-            nums[i] = newnums[i];
-        }
-        
-        
-        return j;
+        return index;
     }
 }
