74c3c5b163bf4df1a63602fd59196bc7544c5caa
Chandana Basavaraj <100051530+CHAB5@users.noreply.github.com>
2024-02-23 03:09:13 -0500

    Time: 214 ms (5.12%), Space: 55.3 MB (29.84%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        int maxFruits = 0;
+        int left = 0, right = 0;
+        Set<Integer> twoFruits = new HashSet<>();
+        ArrayList<Integer> maxValues = new ArrayList<>();
+        
+        if(n==0) return 0;
+        
+        if(n==1) return 1;
+        
+        // if(n>=2) {
+        //     twoFruits.add(fruits[0]);
+        //     twoFruits.add(fruits[1]);
+        //     maxFruits = 2;
+        //     // System.out.println("maxFruits: "+maxFruits);
+        // }
+        
+        while(left<=right && right<n) {
+            
+            if(twoFruits.size() < 2 || twoFruits.contains(fruits[right])) {
+                twoFruits.add(fruits[right]);
+                maxFruits += 1;
+            }
+            else {
+                twoFruits.clear();
+                twoFruits.add(fruits[right]);
+                twoFruits.add(fruits[right-1]);
+                
+                maxFruits += 1;
+                
+                int i=right;
+                while(twoFruits.contains(fruits[i])) {
+                    i--;
+                }
+                
+                left = i+1;
+                
+                maxFruits = right - left + 1;
+                
+                
+                System.out.println(maxFruits);
+            }
+            
+            maxValues.add(maxFruits);
+            right++;
+            
+        }
+        
+        
+        int max = 0;
+        for(int i=0; i< maxValues.size(); i++) {
+            if(maxValues.get(i) > max)
+                max = maxValues.get(i);
+        }       
+        
+        return max;
+    }
+}
