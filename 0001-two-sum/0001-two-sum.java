65eaac883e96b821c2f201f78d37fa458b209083
Chandana Basavaraj <100051530+CHAB5@users.noreply.github.com>
2024-09-18 13:51:27 -0400

    Time: 31 ms (53.04%), Space: 44.6 MB (78.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] twoSum = {-1, -1};
+        int[] twoSum = {0,0};
+        // Store diff between target and current element
         int temp;
-        for(int i=0; i<nums.length; i++) {
+        for(int i=0; i < nums.length; i++) {
             temp = target - nums[i];
-            for(int j=i+1; j<nums.length; j++) {
+            for(int j=i+1; j < nums.length; j++) {
                 if(temp == nums[j]) {
                     twoSum[0] = i;
                     twoSum[1] = j;
@@ -15,3 +16,20 @@ class Solution {
         return twoSum;
     }
 }
+
+
+
+
+// int[] twoSum = {-1, -1};
+        // int temp;
+        // for(int i=0; i<nums.length; i++) {
+        //     temp = target - nums[i];
+        //     for(int j=i+1; j<nums.length; j++) {
+        //         if(temp == nums[j]) {
+        //             twoSum[0] = i;
+        //             twoSum[1] = j;
+        //             return twoSum;
+        //         }
+        //     }
+        // }
+        // return twoSum;
