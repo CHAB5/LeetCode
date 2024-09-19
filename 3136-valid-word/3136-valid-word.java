6a034dcedaa646c193e3a05982108e39ff50b397
Chandana Basavaraj <100051530+CHAB5@users.noreply.github.com>
2024-09-19 19:32:27 -0400

    Time: 1 ms (97.94%), Space: 41.5 MB (96.15%) - LeetHub

--- 3136-valid-word/3136-valid-word.java
+++ 3136-valid-word/3136-valid-word.java
@@ -1,35 +1,27 @@
 class Solution {
     public boolean isValid(String word) {
-        int vowelFlag = 0;
-        int consonantFlag = 0;
+        boolean isVowelPresent = false;
+        boolean isConsonantPresent = false;
         
         if (word.length() < 3) return false;
         
-        for (int i = 0; i < word.length(); i++) {
-            // contains digits or letters
-            if (word.charAt(i) >= '0' && word.charAt(i) <= '9' ||
-                word.charAt(i) >= 'a' && word.charAt(i) <= 'z' ||
-                word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') { 
-                
-                if(word.charAt(i) == 'a' || word.charAt(i) == 'A' ||
-                  word.charAt(i) == 'e' || word.charAt(i) == 'E' ||
-                  word.charAt(i) == 'i' || word.charAt(i) == 'I' ||
-                  word.charAt(i) == 'o' || word.charAt(i) == 'O' ||
-                  word.charAt(i) == 'u' || word.charAt(i) == 'U') {
-                    vowelFlag = 1;
-                } else if(!(word.charAt(i) >= '0' && word.charAt(i) <= '9')) {
-                    consonantFlag = 1;
+        
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) { 
+                if("aeiouAEIOU".indexOf(c) != -1) {
+                    isVowelPresent = true;
+                } else {
+                    isConsonantPresent = true;
                 }
             }
-            else {
+            else if(!Character.isDigit(c)) {
                 return false;
             }
         }
         
-        if(vowelFlag == 1 && consonantFlag == 1) {
+        if(isVowelPresent && isConsonantPresent) 
             return true;
-        } else {
+        else
             return false;
-        }
     }
 }
