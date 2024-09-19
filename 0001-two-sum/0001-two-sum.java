768c1d84f56f7edd812ad9d914c402726dfb8469
Chandana Basavaraj <100051530+CHAB5@users.noreply.github.com>
2024-09-19 12:26:15 -0400

    Time: 4 ms (63.63%), Space: 44.3 MB (98.70%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,42 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+        
+//         // Store diff between target and current element
+//         int temp;
+        
+        
+//         for(int i=0; i < nums.length; i++) {            
+//             //Compare difference with the remaining entries to find solution
+//             for(int j=i+1; j < nums.length; j++) {
+//                 //Entry is equal to the difference
+//                 if(target - nums[i] == nums[j]) {
+//                     return new int[] {i,j};
+//                 }
+//             }
+//         }
+//         return new int[] {};
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] twoSum = {0,0};
         
-        // Store diff between target and current element
-        int temp;
+        Map<Integer, Integer> map = new HashMap<>();
         
+        //Add elements as key and their indices as values in a hash table
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
         
-        for(int i=0; i < nums.length; i++) {
-            temp = target - nums[i];
-            
-            //Compare difference with the remaining entries to find solution
-            for(int j=i+1; j < nums.length; j++) {
-                //Entry is equal to the difference
-                if(temp == nums[j]) {
-                    twoSum[0] = i;
-                    twoSum[1] = j;
-                    return twoSum;
-                }
+        //Find the complement and return both indices
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] {i,map.get(complement)};
             }
         }
-        return twoSum;
+        
+        //In case there is no solution, return an empty array
+        return new int[] {};
     }
 }
