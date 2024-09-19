a6e6dad13b58caa854ca4e9513c7ae211578f208
Chandana Basavaraj <100051530+CHAB5@users.noreply.github.com>
2024-09-19 12:36:47 -0400

    Time: 2 ms (98.55%), Space: 45.2 MB (10.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -18,25 +18,45 @@
 //     }
 // }
 
-class Solution {
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+        
+//         Map<Integer, Integer> map = new HashMap<>();
+        
+//         //Add elements as key and their indices as values in a hash table
+//         for(int i = 0; i < nums.length; i++) {
+//             map.put(nums[i],i);
+//         }
+        
+//         //Find the complement and return both indices
+//         for(int i = 0; i < nums.length; i++) {
+//             int complement = target - nums[i];
+//             if(map.containsKey(complement) && map.get(complement) != i) {
+//                 return new int[] {i,map.get(complement)};
+//             }
+//         }
+        
+//         //In case there is no solution, return an empty array
+//         return new int[] {};
+//     }
+// }
+
+
+class Solution{
     public int[] twoSum(int[] nums, int target) {
         
         Map<Integer, Integer> map = new HashMap<>();
         
-        //Add elements as key and their indices as values in a hash table
-        for(int i = 0; i < nums.length; i++) {
-            map.put(nums[i],i);
-        }
-        
-        //Find the complement and return both indices
-        for(int i = 0; i < nums.length; i++) {
+        for(int i=0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if(map.containsKey(complement) && map.get(complement) != i) {
-                return new int[] {i,map.get(complement)};
-            }
+            if(map.containsKey(complement)) {
+                return new int[] {i, map.get(complement)};
+            } 
+            map.put(nums[i], i);
         }
         
-        //In case there is no solution, return an empty array
+        //Return an empty array if no solution is found
+        
         return new int[] {};
     }
 }
