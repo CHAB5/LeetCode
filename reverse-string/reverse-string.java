9ae66832170ed044627190ddabc6abffb85a4ad6
Chandana Basavaraj <100051530+CHAB5@users.noreply.github.com>
2024-10-28 14:11:55 -0400

    Time: 0 ms, Memory: 48.6 MB - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -4,9 +4,8 @@ class Solution {
         int i = 0, j = s.length - 1;
         while(i <= j) {
             temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++; j--;
+            s[i++] = s[j];
+            s[j--] = temp;
         }
     }
 }
