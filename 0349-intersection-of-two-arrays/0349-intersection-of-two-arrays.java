252cd9b4e9acaf2adeea38156af8cb2f477e9cbb
Chandana Basavaraj <100051530+CHAB5@users.noreply.github.com>
2024-10-30 17:33:23 -0400

    Time: 4 ms (32.46%), Space: 43.3 MB (49.17%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,27 +1,32 @@
+/*
+unique elements on intersection of nums1 and nums2
+empty array - return an empty array
+
+Map - iterate through nums1 and set the value to 1
+iterate through nums2 and for each element in nums2 
+    check if the map contains the element and if its key value is 1
+        add to result array
+        value to 0 in map
+*/
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> first = new HashSet<>();
-        Set<Integer> intersection = new HashSet<>();
-
+        Map<Integer, Integer> seen = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
         
-        for (int i : nums1) {
-            first.add(i);
+        //Mark values occurring in nums1
+        for (int x : nums1) {
+            seen.put(x, 1);
         }
         
-        for ( int i : nums2 ) {
-            if(first.contains(i)) {
-                intersection.add(i);
+        //iterate through nums2 to check if entries are in seen but not added to the result
+        for ( int x : nums2) {
+            if(seen.containsKey(x) && seen.get(x) == 1) {
+                result.add(x);
+                seen.put(x, 0);
             }
         }
         
-        int[] result = new int[intersection.size()];
-        int j = 0;
-        
-        for ( Integer i : intersection) {
-            result[j] = i;
-            j++;
-        }
-        
-        return result;
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
